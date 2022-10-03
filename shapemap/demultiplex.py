a49b5890cffaccca4206098a4f747f1b9ceb88a9
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2022-10-03 15:03:51 +0200

    demultiplex with cutadapt

--- shapemap/demultiplex.py
+++ shapemap/demultiplex.py
@@ -3,8 +3,11 @@ import subprocess as sp
 import os
 import pandas as pd
 import glob
+import logging
 
-output_pattern = "{output_path}/{prefix}_%.fastq.gz"
+logger = logging.getLogger()
+
+output_pattern = "{output_path}/{prefix}_{{name}}.fastq.gz"
 
 
 # TODO correct path in bbduk
@@ -12,22 +15,116 @@ def basename_without_ext(path):
     return os.path.splitext(os.path.basename(path))[0]
 
 
+def run_cutadapt_trimming(
+    readfq: str, matefq: str = None, outreadfq=None, outmatefq=None
+):
+    cmd = [
+        "cutadapt",
+        "-j",
+        "8",
+        "-a",
+        "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC",
+        "-A",
+        "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT",
+        "-o",
+        outreadfq,
+        "-p",
+        outmatefq,
+        readfq,
+        matefq,
+    ]
+
+    sp.run(cmd)
+
+
+def run_cutadapt_demultiplex(
+    readfq: str, tagfile_path, outreadfq=None, matefq: str = None, outmatefq=None
+):
+    cmd = [
+        "cutadapt",
+        "-j",
+        "8",
+        "-e",
+        "0.15",
+        "--no-indels",
+        "-a",
+        f"file:{tagfile_path}",
+        "-o",
+        outreadfq,
+        "-p",
+        outmatefq,
+        readfq,
+        matefq,
+    ]
+
+    sp.run(cmd)
+
+
 def run_bbduk(readfq: str, matefq: str = None, outreadfq=None, outmatefq=None):
     cmd = [
         "bbduk.sh",
         f"in1={readfq}",
         f"out1={outreadfq}",
-        "k=25",
+        "k=21",
         "mink=8",
         "ktrim=r",
-        "ref=truseq.fa.gz",
-        "hdist=1",
+        "ref=truseq",
+        # "ref=adapters/TruSeq3-PE-2.fa",
+        "hdist=2",
+        "tpe",
+        "tbo",
     ]
 
     if matefq is not None:
         cmd.extend([f"in2={matefq}", f"out2={outmatefq}"])
 
     sp.run(cmd)
+
+
+def run_trimmomatic(
+    readfq: str,
+    matefq: str = None,
+    outreadfq=None,
+    outreadfq_unpaired=None,
+    outmatefq=None,
+    outmatefq_unpaired=None,
+):
+    if matefq:
+        cmd = [
+            "trimmomatic",
+            "PE",
+            "-phred33",
+            readfq,
+            matefq,
+            outreadfq,
+            outreadfq_unpaired,
+            outmatefq,
+            outmatefq_unpaired,
+            "ILLUMINACLIP:adapters/TruSeq3-PE-2.fa:4:20:10",
+            #            "LEADING:3",
+            #            "TRAILING:3",
+            # "SLIDINGWINDOW:4:15",
+            "MINLEN:36",
+        ]
+    else:
+        cmd = [
+            "trimmomatic",
+            "SE",
+            "-phred33",
+            readfq,
+            outreadfq,
+            outreadfq_unpaired,
+            "ILLUMINACLIP:adapters/TruSeq3-SE.fa:2:30:10",
+            "LEADING:3",
+            "TRAILING:3",
+            "SLIDINGWINDOW:4:15",
+            "MINLEN:36",
+        ]
+
+    sp.run(cmd)
+
+
+#
 
 
 def run_fastq_multx(readfq, outreadfq, tagfile_path, matefq=None, outmatefq=None):
@@ -40,8 +137,9 @@ def run_fastq_multx(readfq, outreadfq, tagfile_path, matefq=None, outmatefq=None
 
     cmd = [
         "fastq-multx",
-        # "-e",
-        "-b",
+        "-x",
+        "-e",
+        # "-b",
         "-m1",
         "-B",
         tagfile_path,
@@ -66,7 +164,12 @@ def prepare_tagfile(tagfile, groupcolumn, output_path):
 
     tagfile_path = os.path.join(output_path, "tags.tsv")
     outtag.to_csv(tagfile_path, sep="\t", index=False, header=False)
-    return tagfile_path
+
+    with open(os.path.join(output_path, "tags.fa"), "w") as fd:
+        for rid, row in outtag.iterrows():
+            fd.write(f">{row['name']}\n{row['tag']}\n")
+
+    return tagfile_path[:-4]
 
 
 def demultiplex(
@@ -90,17 +193,37 @@ def demultiplex(
         assert len(R1) == len(R2)
 
     if R2:
+        logger.info("Using Pairwise demultiplexing")
         for readfq, matefq in zip(R1, R2):
             trimmed_readfq = os.path.join(
-                os.path.dirname(readfq), "trimmed_" + os.path.basename(readfq)
+                output_path, "trimmed_" + os.path.basename(readfq)
             )
 
             trimmed_matefq = os.path.join(
-                os.path.dirname(matefq), "trimmed_" + os.path.basename(matefq)
+                output_path, "trimmed_" + os.path.basename(matefq)
             )
-            run_bbduk(
+            # trimmed_readfq_unpaired = os.path.join(
+            #    output_path, "trimmed_unpaired_" + os.path.basename(readfq)
+            # )
+
+            # trimmed_matefq_unpaired = os.path.join(
+            #    output_path, "trimmed_unpaired_" + os.path.basename(matefq)
+            # )
+            logger.info("Trimming end {os.path.basename(readfq)}")
+            run_cutadapt_trimming(
                 readfq, matefq, outreadfq=trimmed_readfq, outmatefq=trimmed_matefq
             )
+            # run_bbduk(
+            #    readfq, matefq, outreadfq=trimmed_readfq, outmatefq=trimmed_matefq
+            # )
+            # run_trimmomatic(
+            #    readfq,
+            #    matefq,
+            #    outreadfq=trimmed_readfq,
+            #    outreadfq_unpaired=trimmed_readfq_unpaired,
+            #    outmatefq=trimmed_matefq,
+            #    outmatefq_unpaired=trimmed_matefq_unpaired,
+            # )
 
             outreadfq = output_pattern.format(
                 output_path=output_path, prefix=basename_without_ext(readfq)
@@ -108,25 +231,52 @@ def demultiplex(
             outmatefq = output_pattern.format(
                 output_path=output_path, prefix=basename_without_ext(matefq)
             )
-            run_fastq_multx(
+            logger.info("Demultiplexing {os.path.basename(readfq)}")
+
+            run_cutadapt_demultiplex(
                 readfq=trimmed_readfq,
                 matefq=trimmed_matefq,
-                tagfile_path=tagfile_path,
+                tagfile_path=tagfile_path + ".fa",
                 outreadfq=outreadfq,
                 outmatefq=outmatefq,
             )
+            # run_fastq_multx(
+            #    readfq=trimmed_readfq,
+            #    matefq=trimmed_matefq,
+            #    tagfile_path=tagfile_path,
+            #    outreadfq=outreadfq,
+            #    outmatefq=outmatefq,
+            # )
     else:
         for readfq in R1:
             trimmed_readfq = os.path.join(
-                os.path.dirname(readfq), "trimmed_" + os.path.basename(readfq)
+                output_path, "trimmed_" + os.path.basename(readfq)
             )
-            run_bbduk(readfq, outreadfq=trimmed_readfq)
+            # trimmed_readfq_unpaired = os.path.join(
+            #    output_path, "trimmed_unpaired" + os.path.basename(readfq)
+            # )
+            logger.info("Trimming end {os.path.basename(readfq)}")
+            # run_trimmomatic(
+            #    readfq,
+            #    outreadfq=trimmed_readfq,
+            #    outreadfq_unpaired=trimmed_readfq_unpaired,
+            # )
+            run_cutadapt_trimming(readfq, outreadfq=trimmed_readfq)
+
+            # run_bbduk(readfq, outreadfq=trimmed_readfq)
             outreadfq = output_pattern.format(
                 output_path=output_path, prefix=basename_without_ext(readfq)
             )
-            run_fastq_multx(
-                readfq=trimmed_readfq, tagfile_path=tagfile_path, outreadfq=outreadfq
+            logger.info("Demultiplexing {os.path.basename(readfq)}")
+            run_cutadapt_demultiplex(
+                readfq=trimmed_readfq,
+                tagfile_path=tagfile_path + ".fa",
+                outreadfq=outreadfq,
             )
+
+            # run_fastq_multx(
+            #    readfq=trimmed_readfq, tagfile_path=tagfile_path, outreadfq=outreadfq
+            # )
 
 
 def main():
